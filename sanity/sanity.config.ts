644a0669b72d3d18f4eb4d3487f28c6222f35294
Leozhr <leonardo.leal202@gmail.com>
2023-11-16 17:33:25 -0300

    :zap: Sanity Setup Configuration

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'sanity'
+import {defineConfig} from 'sanity/lib/exports'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
